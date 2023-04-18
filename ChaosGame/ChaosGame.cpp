978049d58e5155482dcc7c8b1fac4d8d5810cb16
admin <admin@BCS-Ubuntu-18.lrccd.ad.losrios.edu>
2023-04-17 21:14:56 -0700

    Ubuntu

--- ChaosGame/ChaosGame.cpp
+++ ChaosGame/ChaosGame.cpp
@@ -7,20 +7,28 @@ using namespace std;
 int main()
 {
 	VideoMode vm(1920, 1080);
-
 	RenderWindow window(vm, "Chaos Game", Style::Default);
 
-	Texture textureBackground;
 	Font font;
 	font.loadFromFile("fonts/arial.ttf");
+
 	Text text;
 	text.setFont(font);
 
+	//Display intro text
+	text.setString("Please click 3 times to create your triangle. Then, click anywhere to start the animation.");
+	text.setCharacterSize(25);
+	text.setFillColor(Color::White);
+	FloatRect textRect = text.getLocalBounds();
+	text.setOrigin(textRect.left + textRect.width / 2.0f,
+		textRect.top);
+	text.setPosition(1920 / 2.0f, 1080 / 2.0f);
+
+
 	while(window.isOpen())
 	{
-		//Display intro text
-		text.setString("Please click 3 times to create your triangle. Then, click anywhere to start the animation.");
-
+		window.draw(text);
+		window.display();
 	}
 
 	return 0;
