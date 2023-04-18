c46759ca99c4650aed01d0f29a816ca2e7bac50c
admin <admin@BCS-Ubuntu-18.lrccd.ad.losrios.edu>
2023-04-17 20:17:36 -0700

    commit from Ubuntu

---
+++ ChaosGame.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+	VideoMode vm(1920, 1080);
+
+	RenderWindow window(vm, "Chaos Game", Style::Default);
+
+	Texture textureBackground;
+	Font font;
+	font.loadFromFile("arial.ttf");
+	Text text;
+	text.setFont(font);
+
+	while(window.isOpen())
+	{
+		//Display intro text
+		text.setString("Please click 3 times to create your triangle. Then, click anywhere to start the animation.");
+
+	}
+
+	return 0;
+}
